3b8977f3c9716dd46911467eeb08f093516bc4b7
Bryan Yap <bcollie115@gmail.com>
2014-02-14 17:05:55 +0000

    updated parser to include raw xml line

--- index_parser.rb
+++ index_parser.rb
@@ -7,24 +7,27 @@ require 'csv'
 def store(item)
   if item =~ /\d+/
     @index_items << {
-      H0: @H0,
-      H1: @H1,
-      H2: @H2,
+      H0: pretty(@H0.texts),
+      H1: @H1 ? pretty(@H1.texts) : nil,
+      H2: @H2 ? pretty(@H2.texts) : nil,
       page: item.to_i,
-      group: @group
+      group: pretty(@group.texts),
+      raw: (@H2 || @H1 || @H0).to_s
     }
   else
     @index_xref << {
-      H0: @H0,
-      H1: @H1,
-      H2: @H2,
+      H0: pretty(@H0.texts),
+      H1: @H1 ? pretty(@H1.texts) : nil,
+      H2: @H2 ? pretty(@H2.texts) : nil,
       xref: item,
-      group: @group
+      raw: (@H2 || @H1 || @H0).to_s,
+      group: pretty(@group.texts)
     }
   end
 end
 
 def pretty(texts)
+  return if texts.nil?
   texts.join(' ')                 # join any separated texts
        .gsub(/,\s*/, '')          # remove ending commas
        .gsub(/(^\s*|\s*$)/, '')   # remove trailing whitespaces
@@ -34,19 +37,19 @@ end
 def parseElem(elem)
   case elem.name
   when 'H0'
-    @H0 = pretty elem.texts
+    @H0 = elem
     @H1 = nil
     @H2 = nil
 
   when 'H1'
-    @H1 = pretty elem.texts
+    @H1 = elem
     @H2 = nil
 
   when 'H2'
-    @H2 = pretty elem.texts
+    @H2 = elem
 
   when 'GROUP'
-    @group = pretty elem.texts
+    @group = elem
     @H0 = nil
 
   when 'INDEX'
@@ -86,14 +89,16 @@ end
 # puts @index_items
 
 CSV.open("output_index.csv", "wb") do |csv|
+  csv << [ 'Group', 'H0', 'H1', 'H2', 'Page', 'Raw XML Line' ]
   @index_items.each do |item|
-    csv << [ item[:group], item[:H0], item[:H1], item[:H2], item[:page].to_s ]
+    csv << [ item[:group], item[:H0], item[:H1], item[:H2], item[:page].to_s, "\"#{item[:raw]}\"" ]
   end
 end
 
 CSV.open("output_xref.csv", "wb") do |csv|
+  csv << [ 'Group', 'H0', 'H1', 'H2', 'Page', 'Raw XML Line' ]
   @index_xref.each do |xref|
-    csv << [ xref[:group], xref[:H0], xref[:H1], xref[:H2], xref[:xref] ]
+    csv << [ xref[:group], xref[:H0], xref[:H1], xref[:H2], xref[:xref], "\"#{xref[:raw]}\"" ]
   end
 end
 
